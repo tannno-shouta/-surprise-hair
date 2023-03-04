92df9238f2372a647ac23ca49656b9c7171cf36a
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2023-03-04 10:25:28 +0900

    メイン画像の表示

--- src/components/Firstview.jsx
+++ src/components/Firstview.jsx
@@ -3,7 +3,9 @@ import styles from 'src/components/Main.module.css'
 export function Firstview (props) {
   return (
       <div className= {styles.firstview}>
-        <div className= {styles.firstview_image} data-image=""></div>
+        <div className= {styles.firstview_image} >
+          <img src='src/img/main_view.jpg' alt ="メイン"></img>
+        </div>
       </div>
   );
 }
