48d9f0a777431610a0eb1ab43f518e2ac8134f20
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2023-05-04 15:58:07 +0900

    Conceptコンポーネントの追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import styles from 'src/components/Main.module.css'
 import { FirstView } from 'src/components/FirstView'
+import { Concept } from 'src/components/Concept';
 
 
 const Main = (props) => {
   return (
       <main className= {styles.main}>
         <FirstView/>
+        <Concept/>
       </main>
   );
 }
