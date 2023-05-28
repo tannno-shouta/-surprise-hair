39d22c09dd8594ad0a83b983f56b1a7ad35b6fd4
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2023-05-28 15:31:38 +0900

    Menuコンポーネントの追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import styles from 'src/components/Main.module.css'
 import { FirstView } from 'src/components/FirstView'
 import { Concept } from 'src/components/Concept';
 import { Features } from 'src/components/Features';
+import { Menu } from 'src/components/Menu/Menu';
 
 const Main = (props) => {
   return (
@@ -9,6 +10,7 @@ const Main = (props) => {
         <FirstView/>
         <Concept/>
         <Features/>
+        <Menu/>
       </main>
   );
 }
