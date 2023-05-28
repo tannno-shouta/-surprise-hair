61384e1079941a474129884e69a42c71f3348fc0
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2023-05-28 15:31:11 +0900

    リファクタリング

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,57 +1,92 @@
-import styles from 'src/components/Feature.module.css';
+import styles from "src/components/Feature.module.css";
 
-const Feature  = (props) =>{
+var foo = 123;
+
+const SALES = [
+  {
+    img: (
+      <img
+        src="/img/feature1.jpg"
+        alt="コンセプト"
+        width="320px"
+        height="480px"
+      ></img>
+    ),
+    title: "技術",
+    text1: "伸びても気にならないグラデーション",
+    text2: "カラー、骨格にあわせたカット&パーマなど",
+    text3: "時間が経っても綺麗に素敵でいられる",
+    text4: "再現性の高い技術にこだわっています",
+  },
+
+  {
+    img: (
+      <img
+        src="/img/feature2.jpg"
+        alt="コンセプト"
+        width="320px"
+        height="480px"
+      ></img>
+    ),
+    title: "空間",
+    text1: "席と席の間隔を広くとり、周りを気にせず",
+    text2: "プライベートな空間でリラックスできます。",
+    text3: "シャンプースペースも思わず眠ってしまう",
+    text4: "空間で癒しのひと時をお過ごしください",
+  },
+
+  {
+    img: (
+      <img
+        src="/img/feature3.jpg"
+        alt="コンセプト"
+        width="320px"
+        height="480px"
+      ></img>
+    ),
+    title: "洗髪",
+    text1: "まるでベッドのようなシャンプー台",
+    text2: "ヘッドクッションにより首の負担がありません",
+    text3: "眠気を誘うちょうどいい明るさの空間で",
+    text4: "まさに「夢心地」の時間を過ごしていただけます",
+  },
+
+  {
+    img: (
+      <img
+        src="/img/feature4.jpg"
+        alt="コンセプト"
+        width="320px"
+        height="480px"
+      ></img>
+    ),
+    title: "補修",
+    text1: "お客様に合わせたオーダーメイドトリートメント",
+    text2: "高濃度高濃度オーガニック美容液による",
+    text3: "ヘッドスパで髪だけでなく日々のストレスや心身の",
+    text4: "トラブルを空間で癒しのひと時をお過ごしください",
+  },
+];
+
+const Feature = (props) => {
   return (
-      <div className= {styles.feature} >
-        <div className= {styles.feature_main_body} >
-          <div className= {styles.feature_image} >
-            <img src='/img/feature1.jpg' alt ="コンセプト" width="320px" height=  "480px"></img>
+    <div className={styles.feature}>
+      {SALES.map((sale) => {
+        return (
+          <div className={styles.feature_main_body}>
+            <div className={styles.feature_image}>{sale.img}</div>
+            <div className={styles.feature_body}>
+              <span className={styles.feature_title}>{sale.title}</span>
+              <p className={styles.feature_text}>{sale.text1}</p>
+              <p className={styles.feature_text}>{sale.text2}</p>
+              <p className={styles.feature_text}>{sale.text3}</p>
+              <p className={styles.feature_text}>{sale.text4}</p>
+            </div>
           </div>
-          <div className= {styles.feature_body} >
-            <span className={styles.feature_title}>技術</span>
-            <p className={styles.feature_text}>伸びても気にならないグラデーション</p>
-            <p className={styles.feature_text}>カラー、骨格にあわせたカット&パーマなど</p>
-            <p className={styles.feature_text}>時間が経っても綺麗に素敵でいられる</p>
-            <p className={styles.feature_text}>再現性の高い技術にこだわっています。</p>
-          </div>
-        </div>
-        <div className= {styles.feature_main_body} >
-          <div className= {styles.feature_body} >
-            <span className={styles.feature_title}>空間</span>
-            <p className={styles.feature_text}>席と席の間隔を広くとり、周りを気にせず</p>
-            <p className={styles.feature_text}>プライベートな空間でリラックスできます。</p>
-            <p className={styles.feature_text}>シャンプースペースも思わず眠ってしまう</p>
-            <p className={styles.feature_text}>空間で癒しのひと時をお過ごしください</p>
-          </div>
-          <div className= {styles.feature_image} >
-            <img src='/img/feature2.jpg' alt ="コンセプト" width="320px" height=  "480px"></img>
-          </div>
-        </div>
-        <div className= {styles.feature_main_body} >
-          <div className= {styles.feature_image} >
-            <img src='/img/feature3.jpg' alt ="コンセプト" width="320px" height=  "480px"></img>
-          </div>
-          <div className= {styles.feature_body} >
-            <span className={styles.facility_title}>シャンプー台</span>
-            <p className={styles.feature_text}>まるでベッドのようなシャンプー台</p>
-            <p className={styles.feature_text}>ヘッドクッションにより首の負担がありません</p>
-            <p className={styles.feature_text}>まさに「夢心地」の時間を過ごしていただけます</p>
-          </div>
-        </div>
-        <div className= {styles.feature_main_body} >
-          <div className= {styles.feature_body} >
-            <span className={styles.treatment_title}>トリートメント</span>
-            <p className={styles.feature_text}>お客様に合わせたオーダーメイドトリートメント</p>
-            <p className={styles.feature_text}>髪につける高濃度高濃度オーガニック美容液で</p>
-            <p className={styles.feature_text}>ヘッドスパで日々のストレスや心身のトラブルを</p>
-            <p className={styles.feature_text}>ほぐす癒しのひと時を提供します。</p>
-          </div>
-          <div className= {styles.feature_image} >
-            <img src='/img/feature4.jpg' alt ="コンセプト" width="320px" height=  "480px"></img>
-          </div>
-        </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export { Feature };
